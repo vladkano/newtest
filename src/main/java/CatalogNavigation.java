bfe9d86ebf53382ea86d54cfe2d52106e5e00cce
Owenkvist <ran.owen@rambler.ru>
2020-09-30 19:39:17 +0500

    PD-145 Реализованы автотесты работоспособности кнопки "Показать ещё" и постраничной навигации на страницах каталога

---
+++ src/main/java/CatalogNavigation.java
@@ -0,0 +1,235 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogNavigation {
+    private static DBWorker worker = new DBWorker();
+    private WebDriver driver;
+
+    By showMoreButton = By.xpath("//span[text()='Показать ещё']");
+    By numberOfPages = By.xpath("//button[@class='pagination__link'][6]");
+
+    By ringImageLink = By.xpath("//span[2]//img[@alt='Малое серебряное кольцо-шарик с черным ситаллом, из коллекции Lollipops (18)']");
+    By ringImageHeader = By.xpath("//h1[text()='Малое серебряное кольцо-шарик с черным ситаллом, из коллекции Lollipops (18)']");
+
+    By ringNameLink = By.xpath("//a[text()='Кольцо Titia с прямоугольными кристаллами (15)']");
+    By ringNameHeader = By.xpath("//h1[text()='Кольцо Titia с прямоугольными кристаллами (15)']");
+
+    By ringDesignerLink = By.xpath("//div[@class='catalog-card__designer']/a[@href='/catalog/rings/pokrytoe_serebrom_kolco_etty_s_lazuritom_175']");
+    By ringDesignerHeader = By.xpath("//b[@class='product-main-info__designer-name']");
+
+    public CatalogNavigation(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    public CatalogNavigation clickOnRingImageLink() {
+        driver.findElement(ringImageLink).click();
+        return this;
+    }
+
+    public String getRingImageHeader() {
+        return driver.findElement(ringImageHeader).getText();
+    }
+
+    public CatalogNavigation clickOnRingNameLink() {
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].click();", driver.findElement(ringNameLink));
+        return this;
+    }
+
+    public String getRingNameHeader() {
+        return driver.findElement(ringNameHeader).getText();
+    }
+
+    public CatalogNavigation clickOnRingDesignerLink() {
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].click();", driver.findElement(ringDesignerLink));
+        return this;
+    }
+
+    public String getRingDesignerHeader() {
+        return driver.findElement(ringDesignerHeader).getText();
+    }
+
+    public String getNumberOfPages() {
+        return driver.findElement(numberOfPages).getText();
+    }
+
+    public CatalogNavigation clickOnShowMoreButton() {
+
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].click();", driver.findElement(showMoreButton));
+
+        return this;
+    }
+
+
+    public int countRings() {
+        int id = 0;
+        String query = "SELECT COUNT(*)id from item " +
+                "JOIN designer ON item.designer_id = designer.id " +
+                "JOIN item_sku ON item.id = item_sku.item_id " +
+                "JOIN sku_picture_list ON item_sku.id = sku_picture_list.sku_id " +
+                "where EXISTS (SELECT * FROM item_sku WHERE item_sku.id = sku_picture_list.sku_id and tag_id = 1)" +
+                "and catalog_id=1 and is_archive = 0 and price != 0 and item_sku.url is not null";
+        try {
+            Statement statement = worker.getCon().createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                id = resultSet.getInt("id");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        worker.getSession().disconnect();
+
+        return id;
+    }
+
+
+    public List<String> getNames() {
+        String name;
+        List<String> text = new ArrayList<>();
+        String query = "SELECT item_sku.name from item " +
+                "JOIN designer ON item.designer_id = designer.id " +
+                "JOIN item_sku ON item.id = item_sku.item_id " +
+                "JOIN sku_picture_list ON item_sku.id = sku_picture_list.sku_id " +
+                "where EXISTS (SELECT * FROM item_sku WHERE item_sku.id = sku_picture_list.sku_id and tag_id = 1)" +
+                "and catalog_id=1 and is_archive = 0 and price != 0 and item_sku.url is not null";
+
+        try {
+            Statement statement = worker.getCon().createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                name = resultSet.getString("name");
+//                System.out.println(name);
+                text.add(name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        worker.getSession().disconnect();
+//        System.out.println("метод getNames: " + text);
+
+        return text;
+    }
+
+
+    public List<String> getDesigners() {
+        String designer;
+        List<String> text = new ArrayList<>();
+        String query = "SELECT designer.name from item " +
+                "JOIN designer ON item.designer_id = designer.id " +
+                "JOIN item_sku ON item.id = item_sku.item_id " +
+                "JOIN sku_picture_list ON item_sku.id = sku_picture_list.sku_id " +
+                "where EXISTS (SELECT * FROM item_sku WHERE item_sku.id = sku_picture_list.sku_id and tag_id = 1)" +
+                "and catalog_id=1 and is_archive = 0 and price != 0 and item_sku.url is not null";
+        try {
+            Statement statement = worker.getCon().createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                designer = resultSet.getString("name");
+//                System.out.println(designer);
+                text.add(designer);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        worker.getSession().disconnect();
+//        System.out.println("метод getDesigner: " + text);
+
+        return text;
+    }
+
+    public List<Integer> getPrice() {
+        int price;
+        List<Integer> text = new ArrayList<>();
+        String query = "SELECT item_sku.price from item " +
+                "JOIN designer ON item.designer_id = designer.id " +
+                "JOIN item_sku ON item.id = item_sku.item_id " +
+                "JOIN sku_picture_list ON item_sku.id = sku_picture_list.sku_id " +
+                "where EXISTS (SELECT * FROM item_sku WHERE item_sku.id = sku_picture_list.sku_id and tag_id = 1)" +
+                "and catalog_id=1 and is_archive = 0 and price != 0 and item_sku.url is not null";
+
+        try {
+            Statement statement = worker.getCon().createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                price = resultSet.getInt("price");
+//                System.out.println(price);
+                text.add(price / 100);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        worker.getSession().disconnect();
+//        System.out.println("метод getPrice: " + text);
+        return text;
+    }
+
+    public List<Integer> getPicture() {
+        int id;
+        List<Integer> text = new ArrayList<>();
+        String query = "SELECT sku_picture_list.id from item " +
+                "JOIN designer ON item.designer_id = designer.id " +
+                "JOIN item_sku ON item.id = item_sku.item_id " +
+                "JOIN sku_picture_list ON item_sku.id = sku_picture_list.sku_id " +
+                "where EXISTS (SELECT * FROM item_sku WHERE item_sku.id = sku_picture_list.sku_id and tag_id = 1)" +
+                "and catalog_id=1 and is_archive = 0 and price != 0 and item_sku.url is not null";
+
+        try {
+            Statement statement = worker.getCon().createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                id = resultSet.getInt("id");
+//                System.out.println(id);
+                text.add(id);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        worker.getSession().disconnect();
+
+//        System.out.println("метод getPicture: " + text);
+        return text;
+    }
+
+
+//    public static void main(String[] args) {
+//
+////        String query = "SELECT COUNT(*) id from item where catalog_id=1 and is_archive = 0";
+//         String query = "SELECT COUNT(*)id from item " +
+//                "JOIN designer ON item.designer_id = designer.id " +
+//                "JOIN item_sku ON item.id = item_sku.item_id " +
+//                "where catalog_id=1 and is_archive = 0 and price != 0 and item_sku.url is not null";
+//
+////        String query = "SELECT COUNT(*) id from item " +
+////                "JOIN designer ON item.designer_id = designer.id " +
+////                "where EXISTS (SELECT * FROM item_sku WHERE item.id = item_sku.item_id and price != 0 and item_sku.url is not null)" +
+////                " and catalog_id=1 and is_archive = 0";
+//
+//
+//        try {
+//            Statement statement = worker.getCon().createStatement();
+//            ResultSet resultSet = statement.executeQuery(query);
+//
+//            while (resultSet.next()) {
+//                int id = resultSet.getInt("id");
+//                System.out.println(id);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        worker.getSession().disconnect();
+//    }
+
+}
